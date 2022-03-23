719509b8edead3a2662f89654945f5289e7ebe1a
eduardoospina <59977491+eduardoospina@users.noreply.github.com>
2022-03-23 14:30:51 -0500

    Add files via upload

---
+++ taller1arep/src/main/java/edu/escuelaing/arep/SparkWebApp.java
@@ -0,0 +1,28 @@
+package edu.escuelaing.arep;
+import org.omg.CosNaming.IstringHelper;
+
+import static spark.Spark.*;
+
+public class SparkWebApp {
+
+    public static void main(String[] args) {
+        secure(getKeystore(), "123456",null, null);
+
+        port(getPort());
+        get("/hello", (req, res) -> "Hello world");
+    }
+
+    static int getPort() {
+        if (System.getenv("PORT") != null) {
+            return Integer.parseInt(System.getenv("PORT"));
+        }
+        return 5000; //returns default port if heroku-port isn't set(i.e. on localhost)
+    }
+
+    static String getKeystore() {
+        if (System.getenv("KEYSTORE") != null) {
+            return System.getenv("Keystore");
+        }
+        return "Keystores/ecikeystore.p12"; //returns default port if heroku-port isn't set(i.e. on localhost)
+    }
+}
